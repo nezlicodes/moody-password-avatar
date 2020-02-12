bcd915b5263146040ed81f632ddd8318e06fc4e6
nezlicodes <benmeradi.lilyanezli@gmail.com>
2020-02-12 13:26:54 +0100

    adds avatar component and re-styles the app

---
+++ src/components/Avatar.vue
@@ -0,0 +1,118 @@
+<template>
+     <div id="emoji">
+        <div class="face">
+          <div class="ears container">
+             <div class="ear left"></div>
+            <div class="ear right"></div>
+          </div>
+           <div class="eyes container">
+             <div class="eye left"></div>
+            <div class="eye right"></div>
+            <div></div>
+          </div>  
+
+          <div v-bind:class="{happy: isHappy, sad:isSad, start:isOkay}"></div>    
+        </div>
+      </div>
+</template>
+
+<script lang="ts">
+import {Component, Prop ,Vue,} from 'vue-property-decorator';
+
+@Component
+export default class Avatar extends Vue {
+    @Prop(String) private password = '';
+
+    // Computed
+    get isSad (){
+    
+    return ( this.password.length <= 8 ) ?  true: false;
+    }
+
+  get isHappy (){
+    const regex = new RegExp(/[!@#$%^&*)(+=._-]/g);
+    return( this.password.length > 8 && regex.test(this.password) )? true: false;
+  }
+
+  get isOkay() {
+    const regex = new RegExp(/[a-zA-Z0-9]/g)
+    const SPEregex = new RegExp(/[!@#$%^&*)(+=._-]/g);
+    return (this.password.length > 8 && regex.test(this.password) && !SPEregex.test(this.password)) ? true: false;
+  }
+  
+}
+</script>
+
+<style lang="css">
+.face {
+  width: 150px;
+  height:150px;
+  border-radius: 50%;
+  background-color: rgb(251,208,67);
+}
+.ear, .eye{
+  height: 0.6em;
+  width: 0.6em;
+  border-radius:50%;
+}
+
+.eye{
+  transform:translateY(40px);
+  animation-name:blink;
+  animation-duration: 1500ms;
+  animation-iteration-count: infinite;
+
+}
+
+.ear{
+ border-bottom: solid 16px #42b983;
+ border-right: solid 9px #42b983;
+  background-color:  #42b983;
+}
+
+.start{
+  position: relative;
+  width: 3.8em;
+  height: 2em;
+  display: flex;
+  justify-content: center;
+  border-top: solid 5px red;
+  align-content: center;
+  transform: translate(20px,70px);
+  border-radius: 10%;
+}
+
+.happy {
+  position: relative;
+  width: 3.8em;
+  height: 2em;
+  display: flex;
+  justify-content: center;
+  border-bottom: solid 10px red;
+  align-content: center;
+  transform: translate(20px,40px);
+  border-radius: 50%;
+}
+
+.sad {
+   position: relative;
+  width: 3.8em;
+  height: 2em;
+  display: flex;
+  justify-content: center;
+  border-top: solid 7px red;
+  align-content: center;
+  transform: translate(25px,70px);
+  border-radius: 50%;
+}
+
+@keyframes blink {
+  from {
+    background-color: white
+  }
+
+  to {
+    background-color: rgba(0,0,0,0.7)
+  }
+}
+</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="hello">
     <h1>Happy password</h1>
-    <div class="container">
+    <div class="container main">
       <div id="form">
         <form>
           <div class="form-group">
@@ -11,7 +11,7 @@
           
           <div class="form-group">
             <label for="password">password:</label>
-            <input type="password" v-model="password" name="password" id="password" placeholder="5upp3r_$t0ng-pa$$w0rd" required>
+               <input type="text" v-model="password" name="password" id="password" placeholder="5upp3r_$t0ng-pa$$w0rd" required>
           </div>
 
           <div class="form-group">
@@ -19,85 +19,79 @@
           </div>
         </form>
       </div>
-      <div id="emoji">
-        <div class="face">
-          <div class="ears container">
-             <div class="ear left"></div>
-            <div class="ear right"></div>
-          </div>
-
-           <div class="eyes container">
-             <div class="eye left"></div>
-            <div class="eye right"></div>
-            <div></div>
-          </div>  
-
-          <div v-bind:class="{happy: isHappy, sad:isSad, start:isOkay}"></div>    
-        </div>
-      </div>
+      <Avatar :password='password' />
     </div>
    
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Component
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import Avatar from './Avatar.vue'
+
+@Component({components:{Avatar}})
+
 export default class HelloWorld extends Vue {
+
+  // data
   private password = '';
   private username = '';
 
+  // Methods
   onSubmit(){
     alert('Hello' + this.username)
     this.password = '';
     this.username = ''
   }
 
-  get isSad (){
-   
-   return ( this.password.length <= 8 ) ?  true: false;
+  // Computed 
+  get passwrd () {
+    return this.password
   }
 
- get isHappy (){
-    const regex = new RegExp(/[!@#$%^&*)(+=._-]/g);
-    return( this.password.length > 8 && regex.test(this.password) )? true: false;
-  }
-
-  get isOkay() {
-    const regex = new RegExp(/[a-zA-Z0-9]/g)
-    const SPEregex = new RegExp(/[!@#$%^&*)(+=._-]/g);
-    return (this.password.length > 8 && regex.test(this.password) && !SPEregex.test(this.password)) ? true: false;
-  }
-  
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
+<style>
 
-.container{display:flex; justify-content:space-around; flex-wrap:wrap; align-content:space-around
+.container {
+  display:flex;
+  justify-content:space-around;
+  flex-wrap:wrap; 
+  align-content:center;
+
 }
 
+ .main {
+max-width: 80%
+}
 .form-group{
   padding: 12px
 }
-input {
+
+input{
   background-color: #fff;
   background-clip: padding-box;
-  border: 1px solid #ced4da;
+  border: 1px solid #42b983;
   padding:.375rem .75rem;
   font-weight: 400;
   width: 100%;
   height: calc(1.5em + .75rem + 2px);
 }
+
+.password{
+  display: flex
+}
+
 label{
   font-size: 18px;
   font-weight: 400;
 }
 
 .btn {
-  display: inline-block;
+    display: inline-block;
     font-weight: 400;
     border: 1px solid transparent;
     padding: .375rem .75rem;
@@ -108,74 +102,4 @@ label{
     background-color: #42b983
 }
 
-.face {
-  width: 150px;
-  height:150px;
-  border: black 1px solid;
-  border-radius: 50%;
-}
-.ear, .eye{
-  height: 0.6em;
-  width: 0.6em;
-  border: 2px solid #42b983;
-  border-radius:50%;
-}
-
-.eye{
-  transform:translateY(40px);
-  animation-name:blink;
-  animation-duration: 1500ms;
-  animation-iteration-count: infinite;
-
-}
-
-.ear{
-  background-color:  #42b983;
-}
-
-.start{
-  position: relative;
-  width: 3.8em;
-  height: 2em;
-  display: flex;
-  justify-content: center;
-  border-top: solid 5px red;
-  align-content: center;
-  transform: translate(20px,70px);
-  border-radius: 10%;
-}
-
-.happy {
-  position: relative;
-  width: 3.8em;
-  height: 2em;
-  display: flex;
-  justify-content: center;
-  border-bottom: solid 5px red;
-  align-content: center;
-  transform: translate(20px,40px);
-  border-radius: 50%;
-}
-
-.sad {
-   position: relative;
-  width: 3.8em;
-  height: 2em;
-  display: flex;
-  justify-content: center;
-  border-top: solid 5px red;
-  align-content: center;
-  transform: translate(20px,70px);
-  border-radius: 50%;
-}
-
-@keyframes blink {
-  from {
-    background-color: white
-  }
-
-  to {
-    background-color: rgba(0,0,0,0.7)
-  }
-}
 </style>
